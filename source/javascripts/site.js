074a1f691f68ba565d6b1968dc6526dc65c9a045
laure peucelle <laurepeu@hotmail.com>
2018-08-09 17:15:18 +0200

    ll

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,27 +1,40 @@
 
 
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+    // window.dataLayer = window.dataLayer || [];
+    // function gtag(){dataLayer.push(arguments);}
+    // gtag('js', new Date());
   
-    gtag('config', 'UA-114780814-1');
+    // gtag('config', 'UA-114780814-1');
 
-    $('#basicModal').modal(options);
-    var options = {
-        'backdrop' : 'static',
-        'keyboard' : 'true',
-        'focus' : 'true'
-      };
-
-
-      $('.carousel').carousel({
-        interval: 200
-      })
+    // $('#basicModal').modal(options);
+    // var options = {
+    //     'backdrop' : 'static',
+    //     'keyboard' : 'true',
+    //     'focus' : 'true'
+    //   };
 
 
+    //   $('.carousel').carousel({
+    //     interval: 200
+    //   });
 
+    
+// $(document).ready(function () {
 
+//   // preloader
+//   $(window).load(function(){
+//     $('.preloader').delay(40).fadeOut(50);
+//   })
+
+// })
+
+    
     //   // Make the arrows appear for each slides and each carousel 
     //  $("div.carousel-item").html(function(){
     //     carousel-item.classList.add()
     //  })
+
+    $(document).ready(function () { // makes sure the whole site is loaded 
+      $('#preloader').delay(3000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+      $('body').delay(3000).css({'overflow':'visible'});
+    })
